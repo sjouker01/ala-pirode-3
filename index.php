46f37baaa3bb80fa7351b6d853866d13e05f8c63
sjouker01 <sjoukjonker13@gmail.com>
2023-02-22 09:45:08 +0100

    update van inlog

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body>
 <?php include("php-add-ons\header.php") ?>
 <h1>Inloggen</h1>
-	<form method="post" action="login.php">
+	<form method="post" action="#">
 		<input type="text" name="username">
         <input type="password" name="password">
         <input type="submit" name="login" value="submit">
@@ -19,12 +19,44 @@
   <?php 
 
   include("php-add-ons\conect.php");
+  
+  
+  
+  
+  // Process form submission
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-  $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-  $result = mysqli_query($conn, $sql);
+      $username = $_POST["username"];
+      $password = $_POST["password"];
+      
+      // Query the database for the entered username and password
+      $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+      $result = mysqli_query($conn, $sql);
+      
+      if (mysqli_num_rows($result) == 1) {
+          // If the query returns a single row, the login was successful
+          $user = mysqli_fetch_assoc($result);
+          $_SESSION["username"] = $user["username"];
+          $_SESSION["rights"] = $user["rights"];
+          
+          if ($user["rights"] == 2) {
+              // If the user has admin rights, redirect to the admin dashboard
+              header("location: admin_dashboard.php");
+          } else {
+              // If the user has normal rights, redirect to the normal dashboard
+              header("location: normal_dashboard.php");
+          }
+      } else {
+          // If the query returns no rows, the login was unsuccessful
+          $error = "Invalid username or password";
+      }
+  }
+  
+  // Close database connection
+  mysqli_close($conn);
   ?>
+  
+
+
     <?php
     // conection 
     include("php-add-ons\conect.php");
