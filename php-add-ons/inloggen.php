e6db2b24bdeb552fd5d8403ba5e1d8836164b2bb
sjouker01 <sjoukjonker13@gmail.com>
2023-02-23 15:03:27 +0100

    klaar

--- php-add-ons/inloggen.php
+++ php-add-ons/inloggen.php
@@ -7,6 +7,30 @@
     <title>Document</title>
 </head>
 <body>
+<?php 
+include('../php-add-ons\conect.php');
+$query = "SELECT username, password FROM gebruikers";
+$result = $conn->query($query);
 
+// Controleren of er resultaten zijn en deze in variabelen opslaan
+$usernames = array();
+$passwords = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $usernames[] = $row["username"];
+        $passwords[] = $row["password"];
+    }
+} else {
+    echo "0 results";
+}
+
+// De resultaten weergeven
+echo "Gebruikersnamen: " . implode(", ", $usernames) . "<br>";
+echo "Wachtwoorden: " . implode(", ", $passwords);
+
+
+
+?>
 </body>
 </html>
