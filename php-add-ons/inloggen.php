57cd1cd2a65c040cf58abe8b4cea3e06237a287b
sjouker01 <sjoukjonker13@gmail.com>
2023-03-03 09:45:39 +0100

    z

--- php-add-ons/inloggen.php
+++ php-add-ons/inloggen.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet"  href="css/style.css">
+    <link rel="stylesheet" type="text/css"  href="css/style.css">
 </head>
 <body>
 <form id="login-form" action="" method="POST">
