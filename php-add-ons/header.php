f4df35c94bf83460b5ce46f4c17584a6cb6785dd
sjouker01 <sjoukjonker13@gmail.com>
2023-03-23 10:56:37 +0100

    sjouk is klaar

--- php-add-ons/header.php
+++ php-add-ons/header.php
@@ -4,18 +4,18 @@
   <div class="logo">Logo</div>
   <nav>
     <ul>
-      <li><a href="producten.php">producten</a></li>
-      <li><a href="#">Knop 2</a></li>
+      <li><a href="index.php">home</a></li>
+      <li><a href="contact-action.php">contact</a></li>
       <li>
 
-<a href="">bier</a></li>
+<a href="producten.php">producten</a></li>
       <li class="dropdown">
         <a href="#">Dropdown</a>
       
         <ul>
-          <li><a href="#">Item 1</a></li>
-          <li><a href="#">Item 2</a></li>
-          <li><a href="#">Item 3</a></li>
+          <li><a href="index.php">home</a></li>
+          <li><a href="producten.php">producten</a></li>
+          <li><a href="contact-pagina.php">contact</a></li>
           <li><a href="php-add-ons\inloggen.php" id="dropdown-inloggen">Inloggen</a></li>
         </ul>
       </li>
