93f823b9c5eea04f498534cc569d5d75c0d39014
Adnan128021 <117442525+Adnan128021@users.noreply.github.com>
2023-03-28 13:44:20 +0200

    form
    ik heb de form html al af

---
+++ overons.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>FORM</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+<<?php
+require_once("php-add-ons/header.php");
+
+
+?>
+
+
+
+
+
+
+
+<?php
+
+
+
+
+
+
+
+?>
+</body>
+</html>
